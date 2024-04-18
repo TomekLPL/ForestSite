ae5108dcc919f545550d5c6ae027358a83ccf109
Tomek <tomek21002100@gmail.com>
2024-04-18 21:08:00 +0200

    added scrollspy

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ const allNavItems = document.querySelectorAll('.nav__link');
 const navBtnBars = document.querySelector('.burger-btn__bars');
 const allSections = document.querySelectorAll('.section');
 const footerYear = document.querySelector('.footer__year');
-const navItems = document.querySelectorAll('.nav__link-desktop');
+const navItems = document.querySelectorAll('.nav__link-desktop-item');
 const scrollSpySections = document.querySelectorAll('.section');
 
 const handleNav = () => {
@@ -39,5 +39,29 @@ const handleCurrentYear = () => {
 	footerYear.innerText = year;
 };
 
+const handleScrollSpy = () => {
+	if (document.body.classList.contains('main-page')) {
+		const sections = [];
+
+		scrollSpySections.forEach(section => {
+			// console.log(window.scrollY);
+			// console.log(section.offsetTop);
+			// console.log(section.offsetHeight);
+
+			if (window.scrollY <= section.offsetTop + section.offsetHeight - 103) {
+				sections.push(section);
+
+				const activeSection = document.querySelector(`a[class*="nav__link-desktop-item"][href*="${sections[0].id}"]`);
+
+				console.log(activeSection);
+
+				navItems.forEach(item => item.classList.remove('nav__link-desktop-item--active'));
+
+				activeSection.classList.add('nav__link-desktop-item--active');
+			}
+		});
+	}
+};
 handleCurrentYear();
 window.addEventListener('scroll', handleObserver);
+window.addEventListener('scroll', handleScrollSpy);
